ed1feef1357b779cc709c24838ac1e9560971965
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2018-11-09 06:15:30 -0200

    movie db with read me

---
+++ README.md
@@ -0,0 +1,9 @@
+The Movie DB API
+
+- Architecture in use: MVVM
+- API Call: Retrofit With HTTP
+- Tests: JUnit
+
+
+This is the minus one version, due to events out of my control I am developing this app in my part time.
+
--- app/src/main/java/br/com/rodriguesalex/moviedb/models/MDbMovie.kt
+++ app/src/main/java/br/com/rodriguesalex/moviedb/models/MDbMovie.kt
@@ -3,8 +3,8 @@ package br.com.rodriguesalex.moviedb.models
 import com.google.gson.annotations.SerializedName
 
 class MDbMovie {
-    @SerializedName("poster_path") val poster: String? = null
-    @SerializedName("title") val title: String? = null
+    @SerializedName("poster_path") var poster: String? = null
+    @SerializedName("title") var title: String? = null
     @SerializedName("vote_average") val voteAverage: Double? = null
     @SerializedName("release_date") val releaseDate: String? = null
 }
